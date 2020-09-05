67516373a42afc09a02f60442fe990f0491d6902
KZamael <daniel.scharrer@arcor.de>
2020-09-06 00:33:59 +0200

    e-commerce project

---
+++ react-backend/data.js
@@ -0,0 +1,44 @@
+export default {
+    products: [
+        {
+            id: '0',
+            name: 'Slim Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 8
+        },
+        {
+            id: '1',
+            name: 'Fit Shirt',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.1,
+            numReviews: 5
+        },
+        {
+            id: '2',
+            name: 'Hot Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            id: '3',
+            name: 'Ladies Best',
+            category: 'Skirt',
+            image: '/images/d1.jpg',
+            price: 45,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 100
+        }
+    ]
+}
